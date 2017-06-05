c315d014c7481e3e2e6de31640f25f329a5f2068
Marc André Wittorf <stu114055@mail.uni-kiel.de>
2017-06-05 20:12:30 +0200

    Some Error Handling in Repl

--- src/Repl/Main.hs
+++ src/Repl/Main.hs
@@ -1,6 +1,8 @@
 module Repl.Main where
 
 import           System.FilePath
+import           Control.Monad.State
+import           Control.Monad.Catch as MC
 import           Paths_drhaskell
 
 import           System.Console.Haskeline
@@ -11,11 +13,9 @@ import           Repl.Loader
 {-
 
 Current Limitations:
-  - no error handling
   - history is not used
   - no let-constructs
-  - no error handling :/
-  - module reloading
+  - our custom checks and modifications
   - anything, really
 -}
 
@@ -36,11 +36,16 @@ replLoop = do
          liftInput $ replPrint res
          replLoop
 
+initInterpreter :: ReplInterpreter ()
+initInterpreter = do
+  datadir <- liftIO getDataDir
+  set [searchPath := [".", datadir </> "TargetModules"]]
+  setImports ["Prelude"]
+
 main :: IO ()
 main = do
   res <- runRepl $ do
-    datadir <- liftIO getDataDir
-    liftInterpreter $ set [searchPath := [".", datadir </> "TargetModules"]]
+    liftInterpreter initInterpreter
     replLoop
   case res of
        Left err -> putStrLn $ "Error:" ++ (show err)
@@ -48,26 +53,41 @@ main = do
 
 replEval :: String -> Repl (Maybe String)
 replEval q = case q of
-  ":?" -> Just <$> replHelp
-  (':':'l':' ': xs)-> do
-    loadModule xs
-    return $ Just "OK!"
-  (':':'t':' ': xs) -> Just <$> (liftInterpreter $ typeOf xs)
+  ':':xs -> replEvalCommand xs
   _    -> liftInterpreter $ replEvalExp q
-    --Just <$> eval q
-    --interpret q (as :: IO ()) >>= liftIO
-    --return ""
 
 replHelp :: Repl String
 replHelp = return $ unlines [
   ":? - This help",
   ":l - load module",
+  ":r - reload module",
   ":t - evaluate type",
   "expression - evaluate expression" ]
 
 replEvalExp :: String -> ReplInterpreter (Maybe String)
 replEvalExp q = do
-  t <- typeOf q
-  if t == "IO ()"
-     then interpret q (as :: IO ()) >>= liftIO >> return Nothing
-     else Just <$> eval q
+  MC.handleAll (\_ -> do
+                      liftInput $ outputStrLn "Error!"
+                      return Nothing) $ do
+    t <- typeOf q
+    if t == "IO ()"
+      then interpret q (as :: IO ()) >>= liftIO >> return Nothing
+      else Just <$> eval q
+
+replEvalCommand :: String -> Repl (Maybe String)
+replEvalCommand cmd = case cmd of
+  "?" -> Just <$> replHelp
+  ('l':' ': xs)-> do
+    MC.handleAll (\_ -> do
+                        liftInput $ outputStrLn "Could not load file"
+                        return Nothing) $ do
+      loadModule xs
+      return (Just "OK!")
+  ('r':_) -> do
+    md <- gets _filename
+    MC.handleAll (\_ -> do
+                        liftInput $ outputStrLn "Could not load file"
+                        return Nothing) $ do
+      loadModule md
+      return (Just "OK!")
+  ('t':' ': xs) -> Just <$> (liftInterpreter $ typeOf xs)
