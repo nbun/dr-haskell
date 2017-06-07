5356f38f8ca233fd8086a5ebafca940ae5536a2d
stu113973 <stu113973@informatik.uni-kiel-de>
2017-06-07 21:54:59 +0200

    Reworked AbstractHaskell.hs

--- src/TypeInference/AbstractHaskell.hs
+++ src/TypeInference/AbstractHaskell.hs
@@ -27,23 +27,23 @@ module AbstractHaskell where
 --- where modname: name of this module,
 ---       imports: list of modules names that are imported,
 ---       typedecls, opdecls, functions: see below
-data Prog = Prog String [String] [TypeDecl] [FuncDecl] [OpDecl]
+data Prog a = Prog a String [String] [TypeDecl a] [FuncDecl a] [OpDecl a]
 
 --- The data type for representing qualified names.
 --- In AbstractHaskell all names are qualified to avoid name clashes.
 --- The first component is the module name and the second component the
 --- unqualified name as it occurs in the source program.
-type QName = (String, String)
+type QName a = (a,String, String)
 
 --- Data type to specify the visibility of various entities.
-data Visibility = Public    -- exported entity
-                | Private   -- private entity
+data Visibility = Public  -- exported entity
+                  | Private  -- private entity
 
 --- The data type for representing type variables.
 --- They are represented by (i,n) where i is a type variable index
 --- which is unique inside a function and n is a name (if possible,
 --- the name written in the source program).
-type TVarIName = (Int, String)
+type TVarIName  a = (a, Int, String)
 
 --- Data type for representing definitions of algebraic data types
 --- and type synonyms.
@@ -64,17 +64,17 @@ type TVarIName = (Int, String)
 ---
 --- Thus, a data type declaration consists of the name of the data type,
 --- a list of type parameters and a list of constructor declarations.
-data TypeDecl
-  = Type     QName Visibility [TVarIName] [ConsDecl]
-  | TypeSyn  QName Visibility [TVarIName] TypeExpr
-  | Instance QName TypeExpr [Context] [(QName, Rule)]
+data TypeDecl a
+  = Type   a   (QName a) Visibility [TVarIName a] [ConsDecl a]
+  | TypeSyn  a (QName a) Visibility [TVarIName a] (TypeExpr a)
+  | Instance  a (QName a) (TypeExpr a) [Context a] [(QName a, Rule a)]
 
 --- A single type context is class name applied to type variables.
-data Context = Context QName [TVarIName]
+data Context a = Context (QName a) [TVarIName a]
 
 --- A constructor declaration consists of the name and arity of the
 --- constructor and a list of the argument types of the constructor.
-data ConsDecl = Cons QName Int Visibility [TypeExpr]
+data ConsDecl a = Cons  a (QName a) Int Visibility  [TypeExpr a]
 
 
 --- Data type for type expressions.
@@ -84,32 +84,32 @@ data ConsDecl = Cons QName Int Visibility [TypeExpr]
 --- Note: the names of the predefined type constructors are
 ---       "Int", "Float", "Bool", "Char", "IO", "Success",
 ---       "()" (unit type), "(,...,)" (tuple types), "[]" (list type)
-data TypeExpr
-  = TVar TVarIName              -- type variable
-  | FuncType TypeExpr TypeExpr  -- function type t1->t2
-  | TCons QName [TypeExpr]      -- type constructor application
+data TypeExpr a
+  = TVar a (TVarIName a)              -- type variable
+  | FuncType a (TypeExpr a) (TypeExpr a)  -- function type t1->t2
+  | TCons a (QName a) [TypeExpr a]      -- type constructor application
                                 -- (TCons (module,name) arguments)
 
 --- Data type to represent the type signature of a defined function.
 --- The type can be missing or a type with an optional context.
-data TypeSig
+data TypeSig a
   = Untyped
-  | CType [Context] TypeExpr
+  | CType a [Context a] (TypeExpr a)
 
 --- Data type for operator declarations.
 --- An operator declaration "fix p n" in Curry corresponds to the
 --- AbstractHaskell term (Op n fix p).
-data OpDecl = Op QName Fixity Int
+data OpDecl a = Op a (QName a) (Fixity a) Int
 
-data Fixity
-  = InfixOp   -- non-associative infix operator
-  | InfixlOp  -- left-associative infix operator
-  | InfixrOp  -- right-associative infix operator
+data Fixity a
+  = InfixOp a  -- non-associative infix operator
+  | InfixlOp a -- left-associative infix operator
+  | InfixrOp a -- right-associative infix operator
 
 --- Data types for representing object variables.
 --- Object variables occurring in expressions are represented by (Var i)
 --- where i is a variable index.
-type VarIName = (Int, String)
+type VarIName a = (a, Int, String)
 
 --- Data type for representing function declarations.
 ---
@@ -132,65 +132,65 @@ type VarIName = (Int, String)
 --- the source text. The comment could be used
 --- by pretty printers that generate a readable Curry program
 --- containing documentation comments.
-data FuncDecl = Func String QName Int Visibility TypeSig Rules
+data FuncDecl a = Func a String (QName a) Int Visibility (TypeSig a) (Rules a)
 
 --- Rules are either a list of single rules or no rule at all
 --- if then function is defined externally.
-data Rules
-  = Rules [Rule]
-  | External
+data Rules a
+  = Rules a [Rule a]
+  | External a
 
 --- The most general form of a rule. It consists of a list of patterns
 --- (left-hand side), a list of guards ("success" if not present in the
 --- source text) with their corresponding right-hand sides, and
 --- a list of local declarations.
-data Rule = Rule [Pattern] Rhs [LocalDecl]
+data Rule a = Rule a [Pattern a] (Rhs a) [LocalDecl a]
 
-data Rhs
-  = SimpleRhs  Expr
-  | GuardedRhs [(Expr, Expr)]
+data Rhs a
+  = SimpleRhs  a (Expr a)
+  | GuardedRhs a [(Statement a, Expr a)]
 
 --- Data type for representing local (let/where) declarations
-data LocalDecl
-  = LocalFunc FuncDecl                 -- local function declaration
-  | LocalPat  Pattern Expr [LocalDecl] -- local pattern declaration
+data LocalDecl a
+  = LocalFunc a  (FuncDecl a)                 -- local function declaration
+  | LocalPat a (Pattern a) (Rhs a) [LocalDecl a] -- local pattern declaration
 
 --- Data type for representing Haskell expressions.
-data Expr
-  = Var        VarIName          -- variable (unique index / name)
-  | Lit        Literal           -- literal (Integer/Float/Char constant)
-  | Symbol     QName             -- a defined symbol with module and name
-  | Apply      Expr Expr         -- application (e1 e2)
-  | InfixApply Expr QName Expr   -- infix application
-  | Lambda     [Pattern] Expr    -- lambda abstraction
-  | Let        [LocalDecl] Expr  -- local let declarations
-  | DoExpr     [Statement]       -- do expression
-  | ListComp   Expr [Statement]  -- list comprehension
-  | Case       Expr [BranchExpr] -- case expression
-  | Typed      Expr TypeExpr     -- typed expression
-  | IfThenElse Expr Expr Expr    -- if-then-else expression
-  | Tuple      [Expr]
-  | List       [Expr]
+data Expr a
+  = Var      a  (VarIName a)          -- variable (unique index / name)
+  | Lit      a  Literal       -- literal (Integer/Float/Char constant)
+  | Symbol   a  (QName  a)            -- a defined symbol with module and name
+  | Apply    a  (Expr a) (Expr a)         -- application (e1 e2)
+  | InfixApply a (Expr a) (QName a) (Expr a)   -- infix application
+  | Lambda     a [Pattern a] (Expr a)    -- lambda abstraction
+  | Let        a [LocalDecl a] (Expr a)  -- local let declarations
+  | DoExpr     a [Statement a]       -- do expression
+  | ListComp   a (Expr a) [Statement a]  -- list comprehension
+  | Case       a (Expr a) [BranchExpr a]  -- case expression
+  | Typed     a (Expr a) (TypeExpr a)     -- typed expression
+  | IfThenElse a (Expr a) (Expr a) (Expr a)    -- if-then-else expression
+  | Tuple      a [Expr a]
+  | List        a [Expr a]
 
 --- Data type for representing statements in do expressions and
 --- list comprehensions.
-data Statement
-  = SExpr Expr        -- an expression (I/O action or boolean)
-  | SPat Pattern Expr -- a pattern definition
-  | SLet [LocalDecl]  -- a local let declaration
+data Statement a
+  = SExpr a (Expr a)        -- an expression (I/O action or boolean)
+  | SPat a (Pattern a) (Expr a) -- a pattern definition
+  | SLet a [LocalDecl a]  -- a local let declaration
 
 --- Data type for representing pattern expressions.
-data Pattern
-  = PVar VarIName              -- pattern variable (unique index / name)
-  | PLit Literal               -- literal (Integer/Float/Char constant)
-  | PComb QName [Pattern]      -- application (m.c e1 ... en) of n-ary
+data Pattern a
+  = PVar  a (VarIName a)              -- pattern variable (unique index / name)
+  | PLit a Literal             -- literal (Integer/Float/Char constant)
+  | PComb a (QName a) [Pattern a]      -- application (m.c e1 ... en) of n-ary
                                -- constructor m.c (PComb (m,c) [e1,...,en])
-  | PAs VarIName Pattern       -- as-pattern (extended Curry)
-  | PTuple [Pattern]
-  | PList [Pattern]
+  | PAs a (VarIName a) (Pattern a)       -- as-pattern (extended Curry)
+  | PTuple a [Pattern a]
+  | PList a [Pattern a]
 
 --- Data type for representing branches in case expressions.
-data BranchExpr = Branch Pattern Expr
+data BranchExpr a = Branch a (Pattern a) (Expr a)
 
 --- Data type for representing literals occurring in an expression.
 --- It is either an integer, a float, a character, or a string constant.
@@ -199,3 +199,4 @@ data Literal
   | Floatc  Float
   | Charc   Char
   | Stringc String
+
