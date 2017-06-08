d196c4eadedea9c8f32333a1544675944df2403a
Niels Bunkenburg <stu114713@informatik.uni-kiel.de>
2017-06-08 07:49:57 +0200

    Apply checks to original instead of transformed file

--- src/Repl/Loader.hs
+++ src/Repl/Loader.hs
@@ -25,7 +25,7 @@ loadModule fn = do
   liftIO $ writeFile cfn transModule
   -- errors <- liftIO $ runChecksL1 cfn
   -- throwM errors
-  errors <- liftIO $ runChecksL1 cfn
+  errors <- liftIO $ runChecksL1 fn
   if null errors
   then do
     liftInterpreter $ loadModules [cfn]
