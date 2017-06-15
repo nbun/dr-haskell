80814d7a08fc446f833002e0d24c5f2ddff0eebc
Jan-Hendrik Matthes <stu113804@mail.uni-kiel.de>
2017-06-15 15:29:11 +0200

    Made some minor modifications to AbstractHaskell

--- src/TypeInference/AbstractHaskell.hs
+++ src/TypeInference/AbstractHaskell.hs
@@ -8,8 +8,7 @@ type VarName = (Int, String)
 
 type Arity = Int
 
-data Visibility = Private
-                | Public
+data Visibility = Private | Public
 
 data Prog a = Prog (MName, a) [(MName, a)] [TypeDecl a] [FuncDecl a]
 
@@ -18,54 +17,55 @@ data TypeDecl a = Type a (QName, a) Visibility [(VarName, a)] [ConsDecl a]
 
 data ConsDecl a = Cons a (QName, a) Arity Visibility [TypeExpr a]
 
-data TypeInfo a = TExpr (TypeExpr a)
-                | TNone
-
 data TypeExpr a = TVar (VarName, a)
                 | FuncType a (TypeExpr a) (TypeExpr a)
                 | TCons a (QName, a) [TypeExpr a]
 
 data TypeSig a = Untyped
-               | CType a (TypeExpr a)
+               | TypeSig (TypeExpr a)
 
-data FuncDecl a = Func a (QName, a) Arity Visibility (TypeSig a) (TypeInfo a) (Rules a)
+data TypeAnn a = TypeAnn (TypeExpr a)
+               | NoAnn
+
+data FuncDecl a
+  = Func a (QName, a) Arity Visibility (TypeSig a) (Rules a)
 
 data Rules a = Rules [Rule a]
-             | External a (TypeExpr a)
+             | External a (TypeAnn a)
 
-data Rule a = Rule a (TypeInfo a) [Pattern a] (Rhs a) [LocalDecl a]
+data Rule a = Rule a (TypeAnn a) [Pattern a] (Rhs a) [LocalDecl a]
 
-data Rhs a = SimpleRhs  a (Expr a)
+data Rhs a = SimpleRhs (Expr a)
            | GuardedRhs a [(Statement a, Expr a)]
 
-data LocalDecl a = LocalFunc a (FuncDecl a)
-                 | LocalPat a (Pattern a) (Rhs a) [LocalDecl a]
+data LocalDecl a = LocalFunc (FuncDecl a)
+                 | LocalPat a (Pattern a) (Expr a) [LocalDecl a]
 
-data Expr a = Var (TypeInfo a) (VarName, a)
-            | Lit (TypeInfo a) (Literal, a)
-            | Symbol (TypeInfo a) (QName, a)
-            | Apply a (TypeInfo a) (Expr a) (Expr a)
-            | InfixApply a (TypeInfo a) (Expr a) (QName, a) (Expr a)
-            | Lambda a (TypeInfo a) [Pattern a] (Expr a)
-            | Let a (TypeInfo a) [LocalDecl a] (Expr a)
-            | DoExpr a (TypeInfo a) [Statement a]
-            | ListComp a (TypeInfo a) (Expr a) [Statement a]
-            | Case a (TypeInfo a) (Expr a) [BranchExpr a]
+data Expr a = Var (TypeAnn a) (VarName, a)
+            | Lit (TypeAnn a) (Literal, a)
+            | Symbol (TypeAnn a) (QName, a)
+            | Apply a (TypeAnn a) (Expr a) (Expr a)
+            | InfixApply a (TypeAnn a) (Expr a) (QName, a) (Expr a)
+            | Lambda a (TypeAnn a) [Pattern a] (Expr a)
+            | Let a (TypeAnn a) [LocalDecl a] (Expr a)
+            | DoExpr a (TypeAnn a) [Statement a]
+            | ListComp a (TypeAnn a) (Expr a) [Statement a]
+            | Case a (TypeAnn a) (Expr a) [BranchExpr a]
             | Typed a (Expr a) (TypeExpr a)
-            | IfThenElse a (TypeInfo a) (Expr a) (Expr a) (Expr a)
-            | Tuple a (TypeInfo a) [Expr a]
-            | List a (TypeInfo a) [Expr a]
+            | IfThenElse a (TypeAnn a) (Expr a) (Expr a) (Expr a)
+            | Tuple a (TypeAnn a) [Expr a]
+            | List a (TypeAnn a) [Expr a]
 
-data Statement a = SExpr a (TypeInfo a) (Expr a)
-                 | SPat a (TypeInfo a) (Pattern a) (Expr a)
-                 | SLet a (TypeInfo a) [LocalDecl a]
+data Statement a = SExpr (Expr a)
+                 | SPat a (Pattern a) (Expr a)
+                 | SLet a [LocalDecl a]
 
-data Pattern a = PVar (TypeInfo a) (VarName, a)
-               | PLit (TypeInfo a) (Literal, a)
-               | PComb a (TypeInfo a) (QName, a) [Pattern a]
-               | PAs a (TypeInfo a) (VarName, a) (Pattern a)
-               | PTuple a (TypeInfo a) [Pattern a]
-               | PList a (TypeInfo a) [Pattern a]
+data Pattern a = PVar (TypeAnn a) (VarName, a)
+               | PLit (TypeAnn a) (Literal, a)
+               | PComb a (TypeAnn a) (QName, a) [Pattern a]
+               | PAs a (TypeAnn a) (VarName, a) (Pattern a)
+               | PTuple a (TypeAnn a) [Pattern a]
+               | PList a (TypeAnn a) [Pattern a]
 
 data BranchExpr a = Branch a (Pattern a) (Expr a)
 
