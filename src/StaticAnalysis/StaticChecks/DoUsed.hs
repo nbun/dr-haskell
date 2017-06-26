9bd15d132f977ee19eefd14c3348ee96ac1c9137
Jonas Busse <jbus@informatik.uni-kiel.de>
2017-06-26 19:40:02 +0200

    cleanup commented DoUsed.hs

--- src/StaticAnalysis/StaticChecks/DoUsed.hs
+++ src/StaticAnalysis/StaticChecks/DoUsed.hs
@@ -4,11 +4,11 @@ import           AstChecks.Check
 import           Language.Haskell.Exts
 import           StaticAnalysis.Messages.StaticErrors
 
-
+-- | Checks if the Module contains any usage of the Do Statement
 doUsed :: Eq l => Module l -> [Error l]
 doUsed (Module _ _ _ _ decls) = mapOverDecls isDoExp decls
 
-
+-- | The actual Do-Occurence-Check
 isDoExp :: Exp l -> [Error l]
 isDoExp (Do l _)  = [DoUsed l]
 isDoExp (MDo l _) = [DoUsed l]
