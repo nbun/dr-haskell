09a85aad5550da0d1142ab22a5f13cc70c82cb0e
Jan-Hendrik Matthes <stu113804@mail.uni-kiel.de>
2017-07-05 21:44:22 +0200

    Added documentation comments to the AbstractHaskell module

--- src/TypeInference/AbstractHaskell.hs
+++ src/TypeInference/AbstractHaskell.hs
@@ -1,3 +1,7 @@
+{-|
+  This library contains data types for representing Haskell programs in Haskell.
+-}
+
 module TypeInference.AbstractHaskell
   ( MName, QName, VarName, Arity, Visibility (..), Prog (..), TypeDecl (..)
   , ConsDecl (..), TypeExpr (..), TypeSig (..), TypeAnn (..), FuncDecl (..)
@@ -9,91 +13,100 @@ module TypeInference.AbstractHaskell
 import Data.List (intercalate)
 
 -- -----------------------------------------------------------------------------
--- Representation of Haskell source code
+-- Representation of Haskell programs
 -- -----------------------------------------------------------------------------
 
--- A module name represented as a string.
+-- | A module name represented as a string.
 type MName = String
 
--- A qualified name represented as a tuple consisting of a module name and a
--- string.
+-- | A qualified name represented as a tuple consisting of a module name and a
+--   string.
 type QName = (MName, String)
 
--- A variable name represented as a tuple consisting of a variable index and a
--- corresponding string representation. The variable index is represented by an
--- integer greater than or equal to zero.
+-- | A variable name represented as a tuple consisting of a variable index and a
+--   corresponding string representation. The variable index is represented by
+--   an integer greater than or equal to zero.
 type VarName = (Int, String)
 
--- The arity of a function or type constructor represented as an integer greater
--- than or equal to zero.
+-- | The arity of a function or type constructor represented as an integer
+--   greater than or equal to zero.
 type Arity = Int
 
--- The visibility of a function, type constructor or type is either private (not
--- exported) or public (exported).
+-- | The visibility of a function, type constructor or type is either private
+--   (not exported) or public (exported).
 data Visibility = Private | Public
   deriving Show
 
--- Representation of a Haskell module consisting of a module name, a list of
--- imported modules, a list of type declarations and a list of function
--- declarations.
+-- | Representation of a Haskell module consisting of a module name, a list of
+--   imported modules, a list of type declarations and a list of function
+--   declarations. The entities can be annotated with any data type.
 data Prog a = Prog (MName, a) [(MName, a)] [TypeDecl a] [FuncDecl a]
   deriving Show
 
--- Representation of an algebraic data type or type synonym declaration.
+-- | Representation of an algebraic data type or type synonym declaration. The
+--   entities can be annotated with any data type.
 data TypeDecl a = Type a (QName, a) Visibility [(VarName, a)] [ConsDecl a]
                 | TypeSyn a (QName, a) Visibility [(VarName, a)] (TypeExpr a)
   deriving Show
 
--- Representation of a type constructor declaration consisting of a type
--- constructor name, the arity and visibility of the type constructor and a list
--- of argument types.
+-- | Representation of a type constructor declaration consisting of a type
+--   constructor name, the arity and visibility of the type constructor and a
+--   list of argument types. The entities can be annotated with any data type.
 data ConsDecl a = Cons a (QName, a) Arity Visibility [TypeExpr a]
   deriving Show
 
--- Representation of a type expression. A type expression is either a type
--- variable, a function type or a type constructor application.
+-- | Representation of a type expression. A type expression is either a type
+--   variable, a function type or a type constructor application. The entities
+--   can be annotated with any data type.
 data TypeExpr a = TVar (VarName, a)
                 | FuncType a (TypeExpr a) (TypeExpr a)
                 | TCons a (QName, a) [TypeExpr a]
   deriving Show
 
--- Representation of a type signature for a function.
+-- | Representation of a type signature for a function. The entities can be
+--   annotated with any data type.
 data TypeSig a = Untyped
                | TypeSig (TypeExpr a)
   deriving Show
 
--- Representation of a type annotation used to annotate the type of expressions.
+-- | Representation of a type annotation used to annotate the type of
+--   expressions. The entities can be annotated with any data type.
 data TypeAnn a = NoTypeAnn
                | TypeAnn (TypeExpr a)
   deriving Show
 
--- Representation of a function declaration consisting of a function name, the
--- arity and visibility of the function, a type signature and a list of rules.
+-- | Representation of a function declaration consisting of a function name, the
+--   arity and visibility of the function, a type signature and a list of rules.
+--   The entities can be annotated with any data type.
 data FuncDecl a = Func a (QName, a) Arity Visibility (TypeSig a) (Rules a)
   deriving Show
 
--- Rules are either a list of single rules or no rule at all if the function is
--- defined externally.
+-- | Rules are either a list of single rules or no rule at all if the function
+--   is defined externally. The entities can be annotated with any data type.
 data Rules a = Rules [Rule a]
              | External a (TypeAnn a)
   deriving Show
 
--- Representation of a function rule consisting of a type annotation, a list of
--- patterns, a right-hand side and a list of local declarations.
+-- | Representation of a function rule consisting of a type annotation, a list
+--   of patterns, a right-hand side and a list of local declarations. The
+--   entities can be annotated with any data type.
 data Rule a = Rule a (TypeAnn a) [Pattern a] (Rhs a) [LocalDecl a]
   deriving Show
 
--- Representation of a rules right-hand side as either guarded or unguarded.
+-- | Representation of a rules right-hand side as either guarded or unguarded.
+--   The entities can be annotated with any data type.
 data Rhs a = SimpleRhs (Expr a)
            | GuardedRhs a [(Expr a, Expr a)]
   deriving Show
 
--- Representation of local 'let' or 'where' declarations.
+-- | Representation of local @let@ or @where@ declarations. The entities can be
+--   annotated with any data type.
 data LocalDecl a = LocalFunc (FuncDecl a)
                  | LocalPat a (Pattern a) (Expr a) [LocalDecl a]
   deriving Show
 
--- Representation of expressions.
+-- | Representation of expressions. The entities can be annotated with any data
+--   type.
 data Expr a = Var (TypeAnn a) (VarName, a)
             | Lit (TypeAnn a) (Literal, a)
             | Symbol (TypeAnn a) (QName, a)
@@ -110,13 +123,15 @@ data Expr a = Var (TypeAnn a) (VarName, a)
             | List a (TypeAnn a) [Expr a]
   deriving Show
 
--- Representation of statements in 'do' expressions or list comprehensions.
+-- | Representation of statements in @do@ expressions or list comprehensions.
+--   The entities can be annotated with any data type.
 data Statement a = SExpr (Expr a)
                  | SPat a (Pattern a) (Expr a)
                  | SLet a [LocalDecl a]
   deriving Show
 
--- Representation of pattern expressions.
+-- | Representation of pattern expressions. The entities can be annotated with
+--   any data type.
 data Pattern a = PVar (TypeAnn a) (VarName, a)
                | PLit (TypeAnn a) (Literal, a)
                | PComb a (TypeAnn a) (QName, a) [Pattern a]
@@ -125,12 +140,13 @@ data Pattern a = PVar (TypeAnn a) (VarName, a)
                | PList a (TypeAnn a) [Pattern a]
   deriving Show
 
--- Representation of branches in 'case' expressions.
+-- | Representation of branches in @case@ expressions. The entities can be
+--   annotated with any data type.
 data BranchExpr a = Branch a (Pattern a) (Expr a)
   deriving Show
 
--- Representation of literals occurring in expressions. It is either an integer,
--- a float, a character, or a string constant.
+-- | Representation of literals occurring in expressions. It is either an
+--   integer, a float, a character, or a string constant.
 data Literal = Intc Int
              | Floatc Float
              | Charc Char
@@ -141,66 +157,72 @@ data Literal = Intc Int
 -- Pretty-printing of abstract Haskell data types
 -- -----------------------------------------------------------------------------
 
--- Representation of pretty-printing options for abstract Haskell data types.
-data AHOptions = AHOptions { currentModule :: String, unqModules :: [String] }
+-- | Representation of pretty-printing options for abstract Haskell data types.
+data AHOptions = AHOptions { currentModule :: String
+                           , unqModules    :: [String] }
 
--- The default pretty-printing options.
+-- | The default pretty-printing options.
 defaultAHOptions :: AHOptions
-defaultAHOptions = AHOptions { currentModule = "", unqModules = [] }
+defaultAHOptions = AHOptions { currentModule = ""
+                             , unqModules    = [] }
 
--- Transforms a qualified name into a string representation.
+-- | Transforms a qualified name into a string representation.
 showQName :: AHOptions -> QName -> String
-showQName opts (mn, n) | mn == (currentModule opts) = n
-                       | elem mn (unqModules opts)  = n
-                       | otherwise                  = mn ++ "." ++ n
+showQName opts (mn, n) | mn == currentModule opts  = n
+                       | elem mn (unqModules opts) = n
+                       | otherwise                 = mn ++ "." ++ n
 
--- Transforms a variable name into a string representation.
+-- | Transforms a variable name into a string representation.
 showVarName :: VarName -> String
 showVarName = snd
 
--- Transforms a type expression into a string representation.
+-- | Transforms a type expression into a string representation.
 showTypeExpr :: AHOptions -> TypeExpr a -> String
 showTypeExpr opts = showTypeExpr' 0
   where
     showTypeExpr' :: Int -> TypeExpr a -> String
     showTypeExpr' _ (TVar (v, _))         = showVarName v
     showTypeExpr' p (FuncType _ t1 t2)
-      = parensIf (p > 0)
-                 ((showTypeExpr' 1 t1) ++ " -> " ++ (showTypeExpr opts t2))
+      = parensIf (p > 0) (showTypeExpr' 1 t1 ++ " -> " ++ showTypeExpr opts t2)
     showTypeExpr' p (TCons _ (qn, _) tes)
-      | (isList qn) && ((length tes) == 1)
-        = "[" ++ (showTypeExpr opts (head tes)) ++ "]"
-      | isTuple qn
+      | isListCons qn && one tes
+        = "[" ++ showTypeExpr opts (head tes) ++ "]"
+      | isTupleCons qn
         = tupled (map (showTypeExpr opts) tes)
       | otherwise
-        = parensIf ((p > 1) && (not (null tes)))
-                   (wsep ((showQName opts qn):(map (showTypeExpr' 2) tes)))
-    wsep :: [String] -> String
-    wsep = intercalate " "
+        = parensIf
+            (p > 1 && not (null tes))
+            (intercalate " " ((showQName opts qn):(map (showTypeExpr' 2) tes)))
 
 -- -----------------------------------------------------------------------------
 -- Definition of helper functions
 -- -----------------------------------------------------------------------------
 
--- Encloses a string in parenthesis if the given condition is true.
+-- | Encloses a string in parenthesis if the given condition is true.
 parensIf :: Bool -> String -> String
 parensIf b s = if b then "(" ++ s ++ ")" else s
 
--- Checks whether the given qualified name is the list type constructor.
-isList :: QName -> Bool
-isList (_, n) = n == "[]"
+-- | Checks whether the given list has exactly one element.
+one :: [a] -> Bool
+one []     = False
+one (_:xs) = null xs
 
--- Checks whether the given qualified name is the tuple type constructor.
-isTuple :: QName -> Bool
-isTuple (_, "")     = False
-isTuple (_, (c:cs)) = (c == '(') && (isTuple' cs)
+-- | Checks whether the given qualified name is the list type constructor.
+isListCons :: QName -> Bool
+isListCons (_, n) = n == "[]"
+
+-- | Checks whether the given qualified name is the tuple type constructor.
+isTupleCons :: QName -> Bool
+isTupleCons (_, "")   = False
+isTupleCons (_, c:cs) = c == '(' && isTupleCons' cs
   where
-    isTuple' :: String -> Bool
-    isTuple' ""           = False
-    isTuple' [x]          = x == ')'
-    isTuple' (x:xs@(_:_)) = (x == ',') && (isTuple' xs)
+    isTupleCons' :: String -> Bool
+    isTupleCons' ""           = False
+    isTupleCons' [x]          = x == ')'
+    isTupleCons' (x:xs@(_:_)) = x == ',' && isTupleCons' xs
 
--- Returns a string representation of a tuple with the given list of components.
+-- | Returns a string representation of a tuple with the given list of
+--   components.
 tupled :: [String] -> String
 tupled []     = "()"
-tupled (x:xs) = "(" ++ x ++ (concatMap (", " ++) xs) ++ ")"
+tupled (x:xs) = "(" ++ x ++ concatMap (", " ++) xs ++ ")"
