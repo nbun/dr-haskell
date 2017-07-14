da4c2e2fe0b5c796f98a0e17c2b60843c87ff7da
Jan-Hendrik Matthes <stu113804@mail.uni-kiel.de>
2017-07-14 18:06:29 +0200

    Added some more functions to the Goodies module

--- src/Goodies.hs
+++ src/Goodies.hs
@@ -1,16 +1,16 @@
 {-|
-  This library provides some useful auxiliary functions
+  This library provides some useful auxiliary functions.
 -}
 
 module Goodies
-  ( both, bothM
+  ( both, bothM, one, parensIf
   ) where
 
 -- -----------------------------------------------------------------------------
 -- Definition of auxiliary functions
 -- -----------------------------------------------------------------------------
 
--- Applies a function to both components of a tuple.
+-- | Applies a function to both components of a tuple.
 both :: (a -> b) -> (a, a) -> (b, b)
 both f (x, y) = (f x, f y)
 
@@ -18,3 +18,12 @@ both f (x, y) = (f x, f y)
 --   monadic actions in the sequence from left to right.
 bothM :: Monad m => (a -> m b) -> (a, a) -> m (b, b)
 bothM f (x, y) = (,) <$> f x <*> f y
+
+-- | Checks whether the given list has exactly one element.
+one :: [a] -> Bool
+one []     = False
+one (_:xs) = null xs
+
+-- | Encloses a string in parenthesis if the given condition is true.
+parensIf :: Bool -> String -> String
+parensIf c s = if c then "(" ++ s ++ ")" else s
