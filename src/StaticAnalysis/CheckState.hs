d53de9a630737cb33a160d44badecbac8d42f3fd
Niels Bunkenburg <stu114713@informatik.uni-kiel.de>
2017-08-16 22:25:55 +0200

    Add CheckState documentation

--- src/StaticAnalysis/CheckState.hs
+++ src/StaticAnalysis/CheckState.hs
@@ -4,50 +4,56 @@ module StaticAnalysis.CheckState (
   Level(..)
 ) where
 
-import           AstChecks.Check
-import           Control.Monad.Catch
-import           Control.Monad.State.Lazy
-import           Data.Functor.Identity
-import           Language.Haskell.Exts
-import           Paths_drhaskell
-import           StaticAnalysis.Messages.StaticErrors
+import AstChecks.Check                                (cId, checkAST,
+                                                       checkASTv2, getAST)
+import Control.Monad.Catch                            (handleAll)
+import Control.Monad.State.Lazy                       (State, StateT, execState,
+                                                       get, put)
+import Data.Functor.Identity                          (Identity)
+import Language.Haskell.Exts                          (Module, SrcSpanInfo)
+import Paths_drhaskell                                (getDataFileName)
 
-import           StaticAnalysis.Messages.Prettify
-import           StaticAnalysis.StaticChecks.DoUsed
-import           StaticAnalysis.StaticChecks.Duplicated
-import           StaticAnalysis.StaticChecks.HigherOrder
-import           StaticAnalysis.StaticChecks.ImportUsed
-import           StaticAnalysis.StaticChecks.Lambda
-import           StaticAnalysis.StaticChecks.ModuleHeadUsed
-import           StaticAnalysis.StaticChecks.NoFunDef
-import           StaticAnalysis.StaticChecks.NoTypeDef
-import           StaticAnalysis.StaticChecks.OwnDataDecl
-import           StaticAnalysis.StaticChecks.Pragma
-import           StaticAnalysis.StaticChecks.Shadowing
-import           StaticAnalysis.StaticChecks.TypeVarApplication
-import           StaticAnalysis.StaticChecks.TypeVars
-import           StaticAnalysis.StaticChecks.Undefined
+import StaticAnalysis.Messages.Prettify               (Level (..))
+import StaticAnalysis.Messages.StaticErrors           (Error)
+import StaticAnalysis.StaticChecks.DoUsed             (doUsed)
+import StaticAnalysis.StaticChecks.Duplicated         (duplicated)
+import StaticAnalysis.StaticChecks.HigherOrder        (checkForHigherOrderFunction)
+import StaticAnalysis.StaticChecks.ImportUsed         (importUsed)
+import StaticAnalysis.StaticChecks.Lambda             (lambdaCheck)
+import StaticAnalysis.StaticChecks.ModuleHeadUsed     (moduleHeadUsed)
+import StaticAnalysis.StaticChecks.NoFunDef           (noFunDef)
+import StaticAnalysis.StaticChecks.NoTypeDef          (noTypeDef)
+import StaticAnalysis.StaticChecks.OwnDataDecl        (ownDataDecl)
+import StaticAnalysis.StaticChecks.Pragma             (pragmaCheck)
+import StaticAnalysis.StaticChecks.Shadowing          (shadowing)
+import StaticAnalysis.StaticChecks.TypeVarApplication (typeVarApplication)
+import StaticAnalysis.StaticChecks.TypeVars           (checkForTypVar)
+import StaticAnalysis.StaticChecks.Undefined          (undef)
 
+-- | State that contains the current module and a list of errors
 data CheckState l = CheckState (Module l) [Error l]
 
 instance Show l => Show (CheckState l) where
   show (CheckState _ es) = unlines $ map show es
 
---prettyCheckState :: CheckState SrcSpanInfo -> String
---prettyCheckState (CheckState _ es) =
---  unlines $ map prettyError es
-
+-- | Runs a check function and adds the result to the list of errors
 check :: (Module l -> [Error l]) -> State (CheckState l) ()
 check check = do
   CheckState m errors <- get
   put $ CheckState m (check m ++ errors)
 
+-- | Runs a check function that accepts external input, such as imported modules
 checkExt :: (Module l -> a -> [Error l]) -> a -> State (CheckState l) ()
 checkExt check ext = do
   CheckState m errors <- get
   put $ CheckState m (check m ext ++ errors)
 
+-- | A Level is a function that combines multiple checks by use of the
+-- CheckState monad
 type LevelT = [Module SrcSpanInfo] -> StateT (CheckState SrcSpanInfo) Identity ()
+
+--------------------------------------------------------------------------------
+-- Level definitions
 
 {- Level 1
 Implemented:
@@ -57,10 +63,10 @@ Implemented:
   ⊕ do Notation verbieten
   ⊕ Eigene Datentypen verbieten
   ⊕ Typsignaturen erzwingen
+  ⊕ Hiding von Prelude Standardfunktionen (z.B.: map, reverse, list, >=, >> etc.)
 
 TODO:
   ⊕ check-expect erzwingen
-  ⊕ Hiding von Prelude Standardfunktionen (z.B.: map, reverse, list, >=, >> etc.)
 -}
 
 levelOne :: LevelT
@@ -84,9 +90,9 @@ levelOne p = do
 Implemented:
   ⊖ Eigene Datentypen erlauben
   ⊕ Typvariablen verbieten
+  ⊕ Showinstanzen für Datentypen generieren
 
 TODO:
-  ⊕ Showinstanzen für Datentypen generieren
   ⊖ Hiding einiger Preludefunktionen aufheben (z.B.: list)
 -}
 
@@ -95,7 +101,7 @@ levelTwo p = do
   check importUsed
   check moduleHeadUsed
   check noFunDef
-  --check typeVarApplication
+  check typeVarApplication
   check doUsed
   checkExt undef p
   checkExt duplicated p
@@ -103,7 +109,7 @@ levelTwo p = do
   check $ checkAST cId cId lambdaCheck cId
   check $ checkASTv2 cId cId cId cId noTypeDef
   check $ checkAST cId shadowing cId cId
-  --check $ checkAST cId cId cId checkForTypVar
+  check $ checkAST cId cId cId checkForTypVar
   check $ checkASTv2 cId cId cId cId pragmaCheck
 
 {- Level 3
@@ -118,7 +124,6 @@ TODO:
 -}
 levelThree :: LevelT
 levelThree p = do
-  --check moduleHeadUsed
   check noFunDef
   check typeVarApplication
   check doUsed
@@ -131,6 +136,7 @@ levelThree p = do
 levelFull :: LevelT
 levelFull _ = return ()
 
+-- | Maps Level to LevelT for easier usage
 levelMapping :: Level -> LevelT
 levelMapping l = case l of
                    Level1    -> levelOne
